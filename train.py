8c4505ebd6a4c90a9650b2f5fad39986a3e46c2b
kcsayem <khalequzzamansayem@gmail.com>
2024-06-05 19:51:59 +0900

    stage and iteration condition for editing image

--- train.py
+++ train.py
@@ -192,22 +192,23 @@ def scene_reconstruction(dataset, opt, hyper, pipe, testing_iterations, saving_i
             image, viewspace_point_tensor, visibility_filter, radii = render_pkg["render"], render_pkg["viewspace_points"], render_pkg["visibility_filter"], render_pkg["radii"]
 
            # edit image
-            edited_image = diffusion_model.edit_image(
-                text_embedding.to(diffusion_model.device),
-                image.unsqueeze(0).to(diffusion_model.device),
-                viewpoint_cam.cached_image.unsqueeze(0).to(diffusion_model.device),
-                guidance_scale=config.guidance_scale,
-                image_guidance_scale=config.image_guidance_scale,
-                diffusion_steps=config.diffusion_steps,
-                lower_bound=config.lower_bound,
-                upper_bound=config.upper_bound,
-            )
+            if iteration > 5000 and stage == "fine":
+                edited_image = diffusion_model.edit_image(
+                    text_embedding.to(diffusion_model.device),
+                    image.unsqueeze(0).to(diffusion_model.device),
+                    viewpoint_cam.cached_image.unsqueeze(0).to(diffusion_model.device),
+                    guidance_scale=config.guidance_scale,
+                    image_guidance_scale=config.image_guidance_scale,
+                    diffusion_steps=config.diffusion_steps,
+                    lower_bound=config.lower_bound,
+                    upper_bound=config.upper_bound,
+                )
 
-            if edited_image.squeeze().size() != image.size():
-                edited_image = torch.nn.functional.interpolate(edited_image, size=image.size()[1:], mode='bilinear')
+                if edited_image.squeeze().size() != image.size():
+                    edited_image = torch.nn.functional.interpolate(edited_image, size=image.size()[1:], mode='bilinear')
 
-            edited_image = edited_image.squeeze().to(viewpoint_cam.original_image.dtype)
-            viewpoint_cam.original_image = edited_image
+                edited_image = edited_image.squeeze().to(viewpoint_cam.original_image.dtype)
+                viewpoint_cam.original_image = edited_image
 
             images.append(image.unsqueeze(0))
 
